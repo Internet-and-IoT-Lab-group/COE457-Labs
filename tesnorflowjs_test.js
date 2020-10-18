a25d7e03f814e3a71fc23d053a57c555a0f9e28f
Saif AlNajjar <sete39@users.noreply.github.com>
2020-10-19 03:45:19 +0400

    changed model to DenseNet121

--- tesnorflowjs_test.js
+++ tesnorflowjs_test.js
@@ -1,5 +1,12 @@
 const tf = require('@tensorflow/tfjs');
+const fs = require('fs')
+async function predictImage() {
+    const model = await tf.loadLayersModel('https://raw.githubusercontent.com/Internet-and-IoT-Lab-group/COE457-Labs/master/EN3_PersonNoPerson_classifier_TfJS/model.json');
 
-const model = tf.loadLayersModel('file://EN3_PersonNoPerson_classifier_TfJS/model.json');
+    // fs.readFile('image.jpg', function(err, data) {
+    //     const result = model.predict(data);
+    //     console.log(model);
+    //  })
+}
 
-const images = tf.fromPixels();
+predictImage();
