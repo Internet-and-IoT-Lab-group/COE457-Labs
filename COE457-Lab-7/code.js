2aac69fb920601d373188fbeb9237c7a7402e4f3
Saif AlNajjar <sete39@users.noreply.github.com>
2020-10-21 18:23:03 +0400

    added jquery code to lab7

--- COE457-Lab-7/code.js
+++ COE457-Lab-7/code.js
@@ -1,3 +1,38 @@
+
 function increase_font() {
-    $(".txt").css("font-size", "48") 
+    var currentFontSize = parseInt($('html').css('font-size'));
+    console.log(currentFontSize);
+    if (currentFontSize < 46) { // maximum font size of 46
+        var newFontSize = currentFontSize + 5;
+        $('html').css('font-size', newFontSize);
+    }
+    console.log(currentFontSize);
+}
+
+function reset_font() {
+    $('html').css('font-size', 15);
+}
+
+function change_font() {
+    if ($('body').css('font-family') === 'Nunito')
+        $('body').css('font-family', 'Comic Sans MS');
+    else
+        $('body').css('font-family', 'Nunito');
+}
+
+function poof() {
+    $('.phonesContainer').hide();
+}
+
+function reduce_image_size() {
+    var width = parseInt($('.imageContainer').css('width'));
+    var height = parseInt($('.imageContainer').css('height'));
+    if (width < 300) { // reset if image is too small
+        $('.imageContainer').css('width', '80%');
+        $('.imageContainer').css('height', '100%')
+    }
+    else {
+        $('.imageContainer').css('width', (width/2.0).toString());
+        $('.imageContainer').css('height', (height/2.0).toString());
+    }   
 }
